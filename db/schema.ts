a5a1c4b673e19632f8e17ff8f209c4a51c8681e2
mmtat <mmtat979@gmail.com>
2024-05-25 09:58:23 +0800

    Drizzle ORM & Neon Database

---
+++ db/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, text } from "drizzle-orm/pg-core";
+
+export const accounts = pgTable("accounts", {
+  id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull(),
+});
---
+++ drizzle/0001_mixed_prowler.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "accounts" ADD COLUMN "plaid_id" text;

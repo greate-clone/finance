740d5c321952978dbdd5f759f4add57651270b6f
mmtat <mmtat979@gmail.com>
2024-05-25 21:29:26 +0800

    Hono improved so simpler

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -9,9 +9,7 @@ import { eq } from "drizzle-orm";
 const app = new Hono().get("/", clerkMiddleware(), async (c) => {
   const auth = getAuth(c);
   if (!auth?.userId) {
-    throw new HTTPException(401, {
-      res: c.json({ error: "Unauthorized" }, 401),
-    });
+    return c.json({ error: "Unauthorized" }, 401);
   }
   const data = await db
     .select({
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,19 +1,11 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { HTTPException } from "hono/http-exception";
 
 import accounts from "./accounts";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
-
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse();
-  }
-  return c.json({ error: "Internal Server Error" }, 500);
-});
 
 const routes = app.route("/accounts", accounts);
 
